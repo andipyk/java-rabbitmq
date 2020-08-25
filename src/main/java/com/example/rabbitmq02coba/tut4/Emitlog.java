32557ce40831c65fa1f41faec565e4de59c712c4
andipyk <Andiokey123>
2020-08-25 12:55:02 +0700

    Initial commit

---
+++ src/main/java/com/example/rabbitmq02coba/tut4/Emitlog.java
@@ -0,0 +1,32 @@
+package com.example.rabbitmq02coba.tut4;
+
+import com.rabbitmq.client.Channel;
+import com.rabbitmq.client.Connection;
+import com.rabbitmq.client.ConnectionFactory;
+
+import java.io.IOException;
+import java.util.concurrent.TimeoutException;
+
+public class Emitlog {
+    private static final String EXCHANGE_NAME = "logs";
+
+    public static void main(String[] args) {
+        ConnectionFactory factory = new ConnectionFactory();
+        factory.setHost("localhost");
+        try (
+                Connection connection = factory.newConnection();
+                Channel channel = connection.createChannel();
+        ) {
+            channel.exchangeDeclare(EXCHANGE_NAME, "fanout");
+            String message = args.length < 1 ? "info: Hello World Message":
+                    String.join(" ", args);
+
+            channel.basicPublish(EXCHANGE_NAME, "", null, message.getBytes("UTF-8"));
+            System.out.println(" [x] Sent '" + message + "'");
+        } catch (TimeoutException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
---
+++ src/test/java/com/example/rabbitmq02coba/Rabbitmq02CobaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.rabbitmq02coba;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Rabbitmq02CobaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
